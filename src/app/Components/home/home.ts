e98c361cfafc3b6481b82c8fccb092ceff8ce374
triilok10 <ittiku3@gmail.com>
2025-09-15 22:27:42 +0530

    Working bootstrap is working

--- src/app/Components/home/home.ts
+++ src/app/Components/home/home.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Header } from "../Common/header/header";
+import { Footer } from "../Common/footer/footer";
+import * as QRCode from 'qrcode';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, Header, Footer],
   templateUrl: './home.html',
-  styleUrl: './home.css'
+  styleUrls: ['./home.css']
 })
-export class Home {
+export class Home implements AfterViewInit {
+  @ViewChild('qrDisplay', { static: true }) qrDisplay!: ElementRef;
+  @ViewChild('qrPlaceholder', { static: true }) qrPlaceholder!: ElementRef;
 
+  ngAfterViewInit(): void {
+    document.getElementById('generate-qr-btn')?.addEventListener('click', () => {
+      this.qrPlaceholder.nativeElement.remove();
+
+      // Correct QR code generation with only `width` in options
+      QRCode.toDataURL('https://cardify.example.com/trilok', { width: 250 })
+        .then((url) => {
+          const img = document.createElement('img');
+          img.src = url;
+          img.classList.add('img-fluid', 'rounded');
+          this.qrDisplay.nativeElement.appendChild(img);
+        })
+        .catch((err) => {
+          console.error('QR Code generation failed:', err);
+        });
+    });
+  }
 }
