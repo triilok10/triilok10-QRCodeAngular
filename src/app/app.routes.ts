580cd005caa1ac1caccfd3e5b8b485c56860b096
triilok10 <ittiku3@gmail.com>
2025-09-15 22:07:29 +0530

    Working on the Card Generations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { Home } from './Components/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component : Home
+  }
+];
