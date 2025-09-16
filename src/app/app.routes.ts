b8af79f9df4a00959a201bb8c1dc409c9960f909
triilok10 <ittiku3@gmail.com>
2025-09-16 11:09:59 +0530

    Create the Login and Signin Components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { Home } from './Components/home/home';
+import { Login } from './Components/Login/login/login';
+import { Register } from './Components/Login/register/register';
 
 export const routes: Routes = [
   {
     path: '',
-    component : Home
-  }
+    component: Home,
+  },
+  {
+    path: 'login',
+    component: Login,
+  },
+  {
+    path: 'register',
+    component: Register,
+  },
 ];
