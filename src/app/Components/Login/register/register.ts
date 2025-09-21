1570ec217e0e54d4d326a57d64941622cd5a5508
triilok10 <ittiku3@gmail.com>
2025-09-21 19:22:32 +0530

    Register Page

--- src/app/Components/Login/register/register.ts
+++ src/app/Components/Login/register/register.ts
@@ -1,11 +1,38 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [HttpClientModule, CommonModule, ReactiveFormsModule],
   templateUrl: './register.html',
-  styleUrl: './register.css'
+  styleUrl: './register.css',
 })
 export class Register {
+  // Reactive Form
+  registerForm = new FormGroup({
+    Username: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    Fullname: new FormControl('', [Validators.required]),
+    ProfileImage: new FormControl(null), // For file uploads, you often handle this separately
+    Password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    ConfirmPassword: new FormControl('', [Validators.required]),
+    Email: new FormControl('', [Validators.required, Validators.email]),
+    MobileNo: new FormControl('', [Validators.required, Validators.pattern('^[0-9]{10}$')]),
+    State: new FormControl('', [Validators.required]),
+  });
 
+  constructor() {}
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+
+      console.log('Form is valid and ready to submit:', this.registerForm.value);
+
+
+    } else {
+      console.log('Form is invalid. Please check the fields.');
+    }
+  }
 }
